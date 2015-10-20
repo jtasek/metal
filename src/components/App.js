1f3906cebcaf7783e28f7394df470b0a7c42e283
Jakub Tašek <jakub.tasek@gmail.com>
2015-10-21 01:43:37 +0200

    add additional files

---
+++ src/components/App.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export class App extends Component {
+    render(){
+        return <div>
+            <h1>Welcome to Metal!</h1>
+        </div>
+    }
+}
