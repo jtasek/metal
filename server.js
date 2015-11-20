c642ca7b36f3159bf19ee7038866dafaa53efc8f
Jakub Tašek <jakub.tasek@gmail.com>
2015-11-21 00:38:26 +0100

    correct webpack.config names

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import path from 'path';  
-import express from 'express';  
-import webpack from 'webpack';  
+import path from 'path';
+import express from 'express';
+import webpack from 'webpack';
 import webpackMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import config from './webpack.config.dev.js';
 
-const app = express();  
+const app = express();
 const compiler = webpack(config);
 
 app.use(webpackMiddleware(compiler, {
